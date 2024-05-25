50edfc34fe1560efa8f52adb4e94b677785419b7
rc <35706356+rehanschaudhry@users.noreply.github.com>
2024-05-25 15:34:25 -0400

    Update homework_5.sql

--- 03_homework/homework_5.sql
+++ 03_homework/homework_5.sql
@@ -10,9 +10,29 @@ Remove any trailing or leading whitespaces. Don't just use a case statement for 
 
 Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
 
-
+SELECT
+    product_name,
+    CASE
+        WHEN INSTR(product_name, '-') > 0 THEN TRIM(SUBSTR(product_name, INSTR(product_name, '-') + 1))
+        ELSE NULL
+    END AS description
+FROM
+    product;
 
 /* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
+
+SELECT
+    product_name,
+	product_size,
+    CASE
+        WHEN INSTR(product_name, '-') > 0 THEN TRIM(SUBSTR(product_name, INSTR(product_name, '-') + 1))
+        ELSE NULL
+    END AS description
+FROM
+    product
+WHERE 
+	product_size REGEXP'[0-9]'
+
 
 
 
